f5e282cd7cb3a7d6666ad91aee2068cb9f37c754
TiaSary <145559326+TiaSary@users.noreply.github.com>
2024-12-15 03:30:30 +0200

    commit

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Include all files in the `src` directory
-  ],
-  theme: {
-    extend: {}, // Extend Tailwind's default theme here if needed
-  },
-  plugins: [], // Add Tailwind plugins here if needed
-};
